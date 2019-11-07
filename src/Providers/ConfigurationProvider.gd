d4eaaba2a50c63a4af22104c00de5e656c0225d7
Kidoncio <lucas.kidoncio@gmail.com>
2019-11-06 23:27:49 -0300

    Add gravação da configuração

--- src/Providers/ConfigurationProvider.gd
+++ src/Providers/ConfigurationProvider.gd
@@ -18,32 +18,38 @@ func _ready() -> void:
 
 func reset() -> void:
 	configuration.sound_muted = false
+	
 	return
 
 
 func get_from_json(filename: String):
 	var file = File.new()
+	
 	file.open(filename, File.READ)
 	
 	var text: String = file.get_as_text()
-	var data = parse_json(text)
+	var parse: JSONParseResult = JSON.parse(text)
+	var data = parse.result
 	
 	file.close()
 	
 	return data
 
 
-func update_configuration_file(newConfiguration = null) -> void:
-	if newConfiguration:
-		configuration = newConfiguration
-	
+func update_configuration_file() -> void:
 	var file = File.new()
 	
-	if not file.file_exists(self.CONFIGURATION_FILE_PATH):
+	if file.open(self.CONFIGURATION_FILE_PATH, File.WRITE) != 0:
+		print("Error opening file")
+		
 		return
 	
-	file.open(self.CONFIGURATION_FILE_PATH, File.WRITE)
-	file.store_line(to_json(configuration))
+	var configurationJSON = to_json(configuration)
+	
+	if not validate_json(configurationJSON):
+		return
+	
+	file.store_line(configurationJSON)
 	file.close()
 	
 	return
@@ -57,4 +63,5 @@ func toggle_sound_muted() -> void:
 
 func update_sound() -> void:
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), configuration.sound_muted)
+	
 	self.update_configuration_file()
