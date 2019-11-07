9a47c05c2340b16980dc85e3e8470565a92d2688
Kidoncio <lucas.kidoncio@gmail.com>
2019-11-06 23:21:57 -0300

    Add Pause Menu

--- src/Game/Main.gd
+++ src/Game/Main.gd
@@ -3,7 +3,6 @@ extends Node2D
 
 func _ready() -> void:
 	SignalProvider.connect(SignalProvider.GAME_OVER, self, "_on_game_over")
-	SignalProvider.connect(SignalProvider.GAME_RESUMED, self, "_on_game_resumed")
 
 
 func _on_game_over() -> void:
@@ -12,15 +11,7 @@ func _on_game_over() -> void:
 	$GameOverTimer.start()
 
 
-func _on_GameOverTimer_timeout():
+func _on_GameOverTimer_timeout() -> void:
+	PointProvider.points = 0
+	
 	get_tree().change_scene(GameProvider.MENU_SCENE_PATH)
-
-
-func _on_game_resumed():
-	get_tree().paused = false
-
-
-func _on_MenuButton_pressed():
-	emit_signal(SignalProvider.GAME_PAUSED)
-	get_tree().paused = true
-	GameProvider.is_paused = true
--- src/Menu/Pause.gd
+++ src/Menu/Pause.gd
@@ -1,9 +1,35 @@
 extends Control
 
+var is_muted: bool = false
 
 func _ready() -> void:
 	SignalProvider.connect(SignalProvider.GAME_PAUSED, self, "_on_game_paused")
 
 
-func _on_game_paused() -> void:
+func _on_MenuButton_pressed():
+	$VBoxContainer/ScoreLabel.text = "Score: " + String(PointProvider.points)
 	
+	get_tree().paused = true
+	
+	self.visible = true
+
+
+func _on_ResumeButton_pressed() -> void:
+	self.visible = false
+	
+	get_tree().paused = false
+
+
+func _on_QuitButton_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_SoundButton_pressed() -> void:
+	ConfigurationProvider.toggle_sound_muted()
+	
+	var buttonText: String = 'Sound OFF'
+	
+	if ConfigurationProvider.configuration.sound_muted:
+		buttonText = 'Sound ON'
+	
+	$VBoxContainer/SoundButton.text = buttonText
